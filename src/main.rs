e4da2c5b53742295f6994987044f23f9537da9bc
Ryneq <p.rynowiecki@gmail.com>
2018-05-31 08:35:54 +0200

    Add second part of the nn library

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let mut nn = NeuralNetwork::new(vec![2, 2, 1]);
     let question = matrix![0., 1., 0., 1.; 0., 0., 1., 1.];
     let anwser   = matrix![0., 1., 1., 0.];
+    nn.run(vec![0.,1.]);
     nn.learn(question, anwser, 200).unwrap();
     println!("nn: {:?}", nn);
 }
--- src/neural_network/neural_network.rs
+++ src/neural_network/neural_network.rs
@@ -1,7 +1,10 @@
 use rand::{Rng, thread_rng};
-use matrix::{Matrix, BaseMatrix};
+use matrix::{Matrix, BaseMatrix, BaseMatrixMut};
 
-type Mat = Matrix<f64>;
+type Mat    = Matrix<f64>;
+type Vector = Vec<f64>;
+
+const BETA: f64 = 5.0;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct NeuralNetwork {
@@ -10,58 +13,85 @@ pub struct NeuralNetwork {
 
 impl NeuralNetwork {
     pub fn new(layers: Vec<usize>) -> Self {
-        let nn: NN = layers.windows(2)
+        let nn: Vec<Mat> = layers.windows(2)
             .map(|layer| random_matrix(layer[0] + 1, layer[1]))
             .collect();
         NeuralNetwork{ nn }
     }
 
-    pub fn run(&self, mut signal: Mat) -> Result<Mat, String> {
+    pub fn run(&self, signal: Vector) -> Result<Vector, String> {
+        let mut signal = Matrix::new(signal.len(), 1, signal);
         self.nn.iter()
-            .map(|layer| signal = activation_function(&(layer.transpose() * add_bias(&signal))))
+            .map(|layer| signal = Self::run_single_layer(layer, &signal))
             .next();
 
         let last_layer = self.nn.last().ok_or("Neural network does not contain any layer".to_string())?;
-        Ok(activation_function(&(last_layer.transpose() * add_bias(&signal))))
+        Ok(Self::run_single_layer(last_layer, &signal).into_vec())
     }
 
-    pub fn learn(&mut self, input: Mat, output: Mat, reps: usize) -> Result<(), String> {
+    pub fn learn(&mut self, input: Mat, output: Mat, reps: usize) -> Result<(), String> { //not tested
         let examples = number_of_examples(&input, &output)?;
 
         for _ in 0..reps {
             let example = random_example(&input, &output, examples);
-            println!("which: {:?}", example);
+            let signals = self.run_and_return_all_signals(&example.0)?;
+            let delta   = example.1 - signals.last().ok_or("No signals".to_string())?;
+            self.back_propagation(signals, delta);
         }
 
         Ok(())
     }
 
-    // fn run_and_return_all_signals() {
-    //     let mut singals: Vec<Mat> = Vec::new();
-    //     self.nn.iter()
-    //         .map(|layer| signal = NeuralNetwork::run_single_layer(layer, &signal))
-    //         .map(|signal| singals.push(signal.clone()))
-    //         .next();
+    fn back_propagation(&mut self, signals: Vec<Mat>, delta: Mat) { //not tested
+        let learning_rate  = 0.05;
+        let mut delta      = delta.clone();
+        let mut signals    = signals.clone();
 
-    //     let last_layer = self.nn.last().ok_or("Neural network does not contain any layer".to_string())?;
-    //     signal = activation_function(&(last_layer.transpose() * add_bias(&signal)));
-    //     singals.push(signal);
-    //     Ok(signals)
-    // }
+        self.nn.iter_mut().rev()
+            .map(|layer| {
+                let signal = signals.pop().expect("No more signals");
+                let error  = Self::find_error(&signal, &delta);
+                let dnn    = error.transpose() * signals.last().expect("No more signals") * learning_rate;
+                *layer     = layer.clone() - dnn;
+                delta      = remove_bias(&layer) * error;
+            }).next();
+    }
+
+    fn find_error(signal: &Mat, delta: &Mat) -> Mat {
+        let error = Matrix::ones(signal.rows(), signal.cols()) - signal;
+
+        error.elemul(&signal.elemul(&delta)) * BETA
+    }
+
+    fn run_and_return_all_signals(&self, signal: &Mat) -> Result<Vec<Mat>, String> {
+        let mut signal = signal.clone();
+        let mut signals: Vec<Mat> = vec![signal.clone()];
+
+        self.nn.iter()
+            .map(|layer| {
+                signal = Self::run_single_layer(layer, &signal);
+                signals.push(signal.clone())
+            }).next();
+
+        let last_layer = self.nn.last().ok_or("Neural network does not contain any layer".to_string())?;
+        signals.push(Self::run_single_layer(last_layer, &signal));
+
+        Ok(signals)
+    }
 
     fn run_single_layer(layer: &Mat, signal: &Mat) -> Mat {
-        activation_function(&(layer.transpose() * add_bias(signal)))
+        activation_function(layer.transpose() * add_bias(signal))
     }
 }
 
 fn random_matrix(rows: usize, cols: usize) -> Mat {
     let mut rng = thread_rng();
-    let content: Vec<f64> = (0..rows*cols).map(|_| rng.gen_range(-1.,1.)).collect();
- 
+    let content: Vector = (0..rows*cols).map(|_| rng.gen_range(-1.,1.)).collect();
+  
     Matrix::new(rows,cols,content)
 }
 
-fn random_example(input: &Mat, output: &Mat, examples: usize) -> (Mat, Mat) {
+fn random_example(input: &Mat, output: &Mat, examples: usize) -> (Mat, Mat) { // not tested
     let mut rng = thread_rng();
     let which_example = rng.gen_range(0, examples);
 
@@ -71,19 +101,17 @@ fn random_example(input: &Mat, output: &Mat, examples: usize) -> (Mat, Mat) {
     (input, output)
 }
 
-fn activation_function(network: &Mat) -> Mat {
-    let beta = 5.;
-
-    let activation: Vec<f64> = network.data()
+fn activation_function(network: Mat) -> Mat {
+    let activation: Vector = network.data()
         .iter()
-        .map(|a| 1./(1. + (-beta*a).exp()))
+        .map(|a| 1.0/(1.0 + (-BETA*a).exp()))
         .collect();
     
     Matrix::new(network.rows(), network.cols(), activation)
 }
 
 fn add_bias(signal: &Mat) -> Mat {
-    let mut signal_with_bias: Vec<f64> = vec![-1.];
+    let mut signal_with_bias: Vector = vec![-1.];
     signal_with_bias.extend(signal.data().clone());
 
     Matrix::new(signal_with_bias.len(), 1, signal_with_bias)
@@ -110,25 +138,25 @@ mod tests {
         use super::*;
         #[test]
         fn run_test() {
-            let nn: NN = vec![Matrix::ones(3,2), Matrix::ones(3,1)]; // 2-2-1 filled with ones
+            let nn: Vec<Mat> = vec![Matrix::ones(3,2), Matrix::ones(3,1)]; // 2-2-1 filled with ones
             let nn = NeuralNetwork { nn };
 
-            let actuall = nn.run(matrix![0.;1.]).unwrap();
-            let expected: Mat= Matrix::ones(1,1) / 2.;
+            let actual = nn.run(vec![0.0, 1.0]).unwrap();
+            let expected: Vector = vec![0.5];
 
-            assert_eq!(expected, actuall);
+            assert_eq!(expected, actual);
         }
 
-        // #[test]
-        // fn learn_test() {
-        //     let nn: NN = vec![Matrix::ones(3,2), Matrix::ones(3,1)]; // 2-2-1 filled with ones
-        //     let nn = NeuralNetwork { nn };
+        #[test]
+        fn run_and_return_all_signals_test() {
+            let nn: Vec<Mat> = vec![Matrix::ones(3,2), Matrix::ones(3,1)]; // 2-2-1 filled with ones
+            let nn = NeuralNetwork { nn };
 
-        //     let actuall = nn.run(matrix![0.;1.]).unwrap();
-        //     let expected: Mat= Matrix::ones(1,1) / 2.;
+            let actual = nn.run_and_return_all_signals(&matrix![0.0; 1.0]).unwrap();
+            let expected = vec![matrix![0.0; 1.0], matrix![0.5; 0.5], matrix![0.5]];
 
-        //     assert_eq!(expected, actuall);
-        // }
+            assert_eq!(expected, actual);
+        }
     }
 
     mod tools {
@@ -136,30 +164,30 @@ mod tests {
         #[test]
         fn activation_function_test() {
             let matrix: Mat= Matrix::zeros(3,3);
-            let actuall = activation_function(&matrix);
+            let actual = activation_function(matrix);
             let expected: Mat= Matrix::ones(3,3) / 2.;
             
-            assert_eq!(actuall, expected);
+            assert_eq!(actual, expected);
         }
 
         #[test]
         fn add_bias_test() {
             let matrix: Mat = Matrix::zeros(3,1);
-            let actuall = add_bias(&matrix);
-            let expected: Mat = Matrix::new(4, 1, vec![-1.,0.,0.,0.]);
+            let actual = add_bias(&matrix);
+            let expected: Mat = Matrix::new(4, 1, vec![-1.0, 0.0, 0.0, 0.0]);
             
-            assert_eq!(4, actuall.rows());
-            assert_eq!(actuall, expected);
+            assert_eq!(4, actual.rows());
+            assert_eq!(actual, expected);
         }
 
         #[test]
         fn remove_bias_test() {
-            let matrix: Mat = Matrix::new(4, 1, vec![-1.,0.,0.,0.]);
-            let actuall = remove_bias(&matrix);
+            let matrix: Mat = Matrix::new(4, 1, vec![-1.0, 0.0, 0.0, 0.0]);
+            let actual = remove_bias(&matrix);
             let expected: Mat = Matrix::zeros(3,1);
             
-            assert_eq!(3, actuall.rows());
-            assert_eq!(actuall, expected);
+            assert_eq!(3, actual.rows());
+            assert_eq!(actual, expected);
         }
 
         #[test]
